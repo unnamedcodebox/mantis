2948c5dcb309165f15a19aadf8fe9bda4416d190
Myasnikov Alexey <amyasnikov@dev.pkb-rio.com>
2019-05-22 11:17:07 +0300

    fix watcher

--- src/core/Watcher.cpp
+++ src/core/Watcher.cpp
@@ -4,7 +4,7 @@ void mantis::Watcher::setConfiguration(QVariantList map){m_map = map;}
 
 void mantis::Watcher::buttonClicked(int index)
 {
-    qDebug() << index << m_map.value(index).toMap()["deviceList"].toList().size();
+    qDebug() << index << m_map.value(index).toMap()["device_list"].toStringList();
     qDebug() << index << m_map.value(index).toMap()["title"].toString();
     qDebug() << index << m_map.value(index).toMap()["id"].toString();
 }
